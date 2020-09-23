93e4588779b006c384ad832804d5f4c8351539e6
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-23 14:07:58 -0700

    Write and fail test for Spec 3: Work with a .txt file

--- lib/far.rb
+++ lib/far.rb
@@ -5,6 +5,11 @@ class Phrase < String
   end
   
   def far(word1, word2)
+    if @string.include?(".txt")
+      file = File.open(@string)
+      @string = File.read(file)
+      file.close
+    end
     @string.gsub!(word1, word2)
   end
 end
--- spec/far_spec.rb
+++ spec/far_spec.rb
@@ -1,17 +1,23 @@
 require('rspec')
 require('far')
+# require('..document.txt')
+# require('..sentence.txt')
 
 describe(Phrase) do
   describe('#far') do
-    
-  it('replaces a given word with a different given word when given a phrase') do
-      phrase = Phrase.new("bro, ruby is so chill")
-      expect(phrase.far("bro", "dude")).to(eq("dude, ruby is so chill"))
+    it('replaces a given word with a different given word when given a phrase') do
+        phrase = Phrase.new("bro, ruby is so chill")
+        expect(phrase.far("bro", "dude")).to(eq("dude, ruby is so chill"))
     end
-  end
 
-  it('replaces a string pattern with a given word when given a phrase') do
-    phrase = Phrase.new("I am walking my cat to the cathedral.")
-    expect(phrase.far("cat", "dog")).to(eq("I am walking my dog to the doghedral."))
+    it('replaces a string pattern with a given word when given a phrase') do
+      phrase = Phrase.new("I am walking my cat to the cathedral.")
+      expect(phrase.far("cat", "dog")).to(eq("I am walking my dog to the doghedral."))
+    end
+
+    it('works with a .txt file') do
+      phrase = Phrase.new('./sentence.txt')
+      expect(phrase.far("is", "was")).to(eq("this was a sentence"))
+    end
   end
 end
