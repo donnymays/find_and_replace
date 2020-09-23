2648e1d3527d30fe4d017aecec9a304f09ed6a61
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-23 13:25:20 -0700

    Write and fail test for spec 1: Return a string with word replacement

---
+++ lib/far.rb
@@ -0,0 +1,12 @@
+class Phrase < String
+
+  def initialize (string)
+    @string = string
+  end
+  
+  def far(word1, word2)
+    @string.gsub(word1, word2)
+    @string
+  end
+end
+
--- spec/far_spec.rb
+++ spec/far_spec.rb
@@ -1,1 +1,11 @@
-require ('rspec')
+require('rspec')
+require('far')
+
+describe(Phrase) do
+  describe('#far') do
+    it('replaces a given word with a different given word when given a phrase') do
+      phrase = Phrase.new("bro, ruby is so chill")
+      expect(phrase.far("bro", "dude")).to(eq("dude, ruby i so chill"))
+    end
+  end
+end
