edfd056a4cef4729664ada8802abe48ff3ef66c1
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-23 13:34:54 -0700

    Write and pass test for spec 2: Replace partial matches

--- spec/far_spec.rb
+++ spec/far_spec.rb
@@ -3,9 +3,15 @@ require('far')
 
 describe(Phrase) do
   describe('#far') do
-    it('replaces a given word with a different given word when given a phrase') do
+    
+  it('replaces a given word with a different given word when given a phrase') do
       phrase = Phrase.new("bro, ruby is so chill")
       expect(phrase.far("bro", "dude")).to(eq("dude, ruby is so chill"))
     end
+  end
+
+  it('replaces a string pattern with a given word when given a phrase') do
+    phrase = Phrase.new("I am walking my cat to the cathedral.")
+    expect(phrase.far("cat", "dog")).to(eq("I am walking my dog to the doghedral."))
   end
 end
